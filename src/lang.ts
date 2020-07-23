e5bdeaf21edb84d04f7ab12c0a3d4b0e6456af02
xWafl <73sampleperson@gmail.com>
2020-07-23 11:21:50 -0400

    changes

---
+++ src/lang.ts
@@ -0,0 +1,9 @@
+export default {
+    us: "$",
+    au: "$",
+    ca: "CAD ",
+    fr: "€",
+    gb: "£",
+    cn: "￥",
+    jp: "￥"
+}
--- src/webhook.ts
+++ src/webhook.ts
@@ -1,4 +1,5 @@
 import axios from "axios";
+import lang from "./lang";
 
 const urlToEmbed = (prodUrl: string, cheerioData: Record<string, any>[]) => {
     const color = 0x008080;
@@ -43,45 +44,55 @@ const urlToEmbed = (prodUrl: string, cheerioData: Record<string, any>[]) => {
 
 export const sendEmbed = async (data: Record<string, any>, webhookUrl: string) => {
     const color = 0x008080;
-    const title = "New upcoming product!";
-    const url = `https://www.nike.com/${data.publishedContent.marketplace}/launch?s=upcoming`;
+    const title = "New product has been loaded!";
+    const url = `https://nike.com/${data.publishedContent.marketplace}/launch/t/${data.productInfo[0].productContent.slug}`;
     const footer = {
         text: `Project Unknown | Powered by https://discord.gg/24TqAYj | ${new Date().toLocaleString("en-US", { timeZone: "America/New_York" })}`
     };
-    const thumbnail = {
+    const image = {
         url: data.productInfo[0].imageUrls.productImageUrl
     };
     const fields = [
         {
-            name: "**Title**",
+            name: "**Name**",
             value: `[${data.productInfo[0].merchProduct.labelName}](https://nike.com/${data.publishedContent.marketplace}/launch/t/${data.productInfo[0].productContent.slug})`,
             inline: true
         },
         {
-            name: "**Style**",
-            value: `${data.productInfo[0].merchProduct.styleType}`,
+            name: "**Launch type**",
+            value: `${data.productInfo[0].launchView.method}`,
+            inline: true
+        },
+        {
+            name: "**PID**",
+            value: `${data.productInfo[0].merchProduct.pid}`,
             inline: true
         },
         {
             name: "**Price**",
-            value: `$${data.productInfo[0].merchPrice.fullPrice}`,
+            value: `${lang[data.publishedContent.marketplace.toLowerCase() as keyof typeof lang]}${data.productInfo[0].merchPrice.fullPrice}`,
             inline: true
         },
         {
             name: "**Release date**",
-            value: `${new Date(data.productInfo[0].launchView.startEntryDate).toLocaleString("en-US", { timeZone: "America/New_York" })}`
-        }
+            value: `${new Date(data.productInfo[0].launchView.startEntryDate).toLocaleString("en-US", { timeZone: "America/New_York" })}`,
+            inline: true
+        },
+        {
+            name: "**Sizes**",
+            value: `${data.productInfo[0].skus.reduce((acc: string, cur: { nikeSize: string }) => acc + cur.nikeSize + ", ", "")}`
+        },
     ];
-    const embed = { color, title, url, footer, thumbnail, fields };
+    const embed = { color, title, url, footer, image, fields };
 
     console.log(embed);
     try {
-    axios({
-        method: "POST",
-        url: webhookUrl,
-        data: {
-            embeds: [embed]
-        }
-    });
-} catch (e) { console.error(e) }
+        axios({
+            method: "POST",
+            url: webhookUrl,
+            data: {
+                embeds: [embed]
+            }
+        });
+    } catch (e) { console.error(e) }
 };
